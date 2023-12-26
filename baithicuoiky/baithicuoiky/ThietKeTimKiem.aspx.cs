5aa40b9ea6e2994e47e5e47f92ddd6e6471b2f5c
nguyenthicuc2003 <nguyenthicuc250403@gmail.com>
2023-12-26 11:14:12 +0700

    baithi

---
+++ baithicuoiky/baithicuoiky/ThietKeTimKiem.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace baithicuoiky
+{
+    public partial class ThietKeTimKiem : System.Web.UI.Page
+    {
+
+        public static string chuoiKN = "Data Source=DESKTOP-25LT52B\\SQLEXPRESS;Initial Catalog=QL_SINHVIEN;Integrated Security=True;";
+        public static SqlConnection cn = new SqlConnection(chuoiKN);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+        void HienThi()
+        {
+            try
+            {
+                string chuoiSQL = "SELECT * FROM tbl_monhoc";
+                SqlDataAdapter da = new SqlDataAdapter(chuoiSQL, cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            catch (Exception)
+            {
+                Label1.Text = "Lỗi kết nối!";
+            }
+        }
+
+        protected void btnTimTheoMa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtMaTH.Text == "")
+                {
+                    HienThi();
+                }
+                else
+                {
+                    string chuoiSQL = "SELECT * FROM tbl_truong where MaTruong = N'" + txtMaTH.Text + "'";
+                    SqlDataAdapter da = new SqlDataAdapter(chuoiSQL, cn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                    txtMaTH.Text = "";
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+        protected void btnTimTheoTen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtTenTH.Text == "")
+                {
+                    HienThi();
+                }
+                else
+                {
+                    string chuoiSQL = "SELECT * FROM tbl_truong where TenTruong = N'" + txtTenTH.Text + "'";
+                    SqlDataAdapter da = new SqlDataAdapter(chuoiSQL, cn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                    txtTenTH.Text = "";
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
